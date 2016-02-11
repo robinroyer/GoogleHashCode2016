2b1f4d20455a988bf0e19cbea71383246da9a955
Lisa Courant <lisa.courant@gmail.com>
2016-02-11 20:04:44 +0100

    Commande

--- src/main/java/GoogleHashCode2016/HashCode2016/Commands.java
+++ src/main/java/GoogleHashCode2016/HashCode2016/Commands.java
@@ -1,22 +1,26 @@
 package GoogleHashCode2016.HashCode2016;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Created by Lisa on 11/02/2016.
  */
 public class Commands {
     public int id;
+
     //Location de la commande
     public int r;
     public int c;
+
     public ArrayList<Product> listProduct;
+
+    public Map<Integer, Integer> stock;
 
     public Commands(int id, int r, int c){
         this.id = id;
         this.r = r;
         this.c = c;
     }
-
 
 }
--- src/main/java/GoogleHashCode2016/HashCode2016/Product.java
+++ src/main/java/GoogleHashCode2016/HashCode2016/Product.java
@@ -4,4 +4,11 @@ package GoogleHashCode2016.HashCode2016;
  * Created by Lisa on 11/02/2016.
  */
 public class Product {
+    public int id;
+    public int weight;
+
+    public Product(int id, int weight) {
+        this.id = id;
+        this.weight = weight;
+    }
 }
