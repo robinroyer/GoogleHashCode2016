5df94dd52551efefc1593cc8f6ebd4c9ad1d0b30
ccanete <cyril.canete@insa-lyon.fr>
2016-02-11 20:13:46 +0100

    drone methods

--- src/main/java/GoogleHashCode2016/HashCode2016/Drone.java
+++ src/main/java/GoogleHashCode2016/HashCode2016/Drone.java
@@ -1,5 +1,8 @@
 package GoogleHashCode2016.HashCode2016;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static GoogleHashCode2016.HashCode2016.Helpers.getTime;
 
 /**
@@ -10,18 +13,27 @@ public class Drone {
     int r;
     int c;
     int maxWeight;
+    int endWorkTime;
+    List <Commands> commands;
 
     public Drone(int id, int r, int c, int maxWeight) {
         this.id = id;
         this.r = r;
         this.c = c;
         this.maxWeight = maxWeight;
+        this.endWorkTime = 0;
+        this.commands = new ArrayList<Commands>();
     }
 
     public int goTo (int r, int c) {
-        //
         // return time fo travel to position
-        return getTime(this.r, this.c, r, c);
+        int time = getTime(this.r, this.c, r, c);
+        this.endWorkTime += time;
+        return time;
+    }
+
+    public void addCommand (Commands command) {
+        this.commands.add(command);
     }
 
 
