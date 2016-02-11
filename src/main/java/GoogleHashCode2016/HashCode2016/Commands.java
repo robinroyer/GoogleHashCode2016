570c98c895c9a4ce294353bd6cc7317eb4a0b817
ccanete <cyril.canete@insa-lyon.fr>
2016-02-11 20:51:07 +0100

    add getFistProduct

--- src/main/java/GoogleHashCode2016/HashCode2016/Commands.java
+++ src/main/java/GoogleHashCode2016/HashCode2016/Commands.java
@@ -23,6 +23,17 @@ public class Commands {
         this.c = c;
     }
 
+    public int getFirstProductId () {
+        int idProduct = this.stock.keySet().iterator().next();
+        int quantity = this.stock.get(idProduct);
+        if (quantity == 1) {
+            this.stock.remove(idProduct);
+        } else {
+            this.stock.replace(idProduct, quantity - 1);
+        }
+        return idProduct;
+    }
+
 
     public Map<Integer, Integer> getStock() {
         return stock;
