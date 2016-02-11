c08240e446bf56fe8a81b8ffafcb00cdcd52720e
Lisa Courant <lisa.courant@gmail.com>
2016-02-11 19:50:47 +0100

    Order

--- src/main/java/GoogleHashCode2016/HashCode2016/Commands.java
+++ src/main/java/GoogleHashCode2016/HashCode2016/Commands.java
@@ -1,7 +1,22 @@
 package GoogleHashCode2016.HashCode2016;
 
+import java.util.ArrayList;
+
 /**
  * Created by Lisa on 11/02/2016.
  */
 public class Commands {
+    public int id;
+    //Location de la commande
+    public int r;
+    public int c;
+    public ArrayList<Product> listProduct;
+
+    public Commands(int id, int r, int c){
+        this.id = id;
+        this.r = r;
+        this.c = c;
+    }
+
+
 }
